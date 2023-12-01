aa2ae74dfc08e93d74748c7a7f764901d8bb0b0e
Toty8 <80042351+Toty8@users.noreply.github.com>
2023-12-01 15:23:14 +0200

    Revert changes

--- src/api/data.js
+++ src/api/data.js
@@ -1,7 +1,7 @@
 import * as api from './api.js';
 
-const host = 'https://library-catalog-toty8-backend.onrender.com'
-api.settings.host = 'https://library-catalog-toty8-backend.onrender.com';
+const host = 'http://localhost:3030'
+api.settings.host = 'http://localhost:3030';
 
 export const login = api.login;
 export const register = api.register;
